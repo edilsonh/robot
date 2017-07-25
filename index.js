a4948238b40c2757b38b5c1bdbc0e8db128ea32a
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-07-25 15:40:32 -0400

    change extension of index to mustache

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mustacheExpress = require("mustache-express");
+
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", function(req, res) {
+  res.render("index.html");
+});
+
+app.listen(3000);
