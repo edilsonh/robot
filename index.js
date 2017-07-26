d734fcbfc1d00cb688bd8cbb27c5548c653c3dbc
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-07-26 08:21:23 -0400

    created mustache page for individual robots and added some styling to it

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mustacheExpress = require("mustache-express");
+const DATA = require("./views/data")
 
 app.engine("mustache", mustacheExpress());
 app.set("view engine", "mustache");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", function(req, res) {
-  res.render("index.html");
+  res.render("index.mustache", DATA);
+});
+
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.get("/:username", function(req, res) {
+  let foundRobot = DATA.users.find(robo =>
+    robo.username === req.params.username
+  );
+  res.render("robot.mustache", foundRobot);
 });
 
 app.listen(3000);
